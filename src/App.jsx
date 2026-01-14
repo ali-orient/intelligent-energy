05c8c3273e6a1b33989b05e373e9fe92021eb766
Muhammad Ali khan <ali.khan@orient-power.com>
2026-01-14 13:47:29 +0500

    Center align email in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Home from './pages/Home.jsx'
@@ -9,6 +9,10 @@ import Project from './pages/Project.jsx'
 
 export default function App() {
   const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [location.pathname])
 
   return (
     <div className="app-shell">
